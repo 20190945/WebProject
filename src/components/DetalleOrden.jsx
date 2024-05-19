e94fe10352544e9ac208315cf21ccc134206892e
Robert213Z <114684008+Robert213Z@users.noreply.github.com>
2024-05-19 11:39:03 -0500

    DetalleOrden.jsx

--- src/components/DetalleOrden.jsx
+++ src/components/DetalleOrden.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import '../Alumno3y4/DetalleOrden.css'
+import '../components/DetalleOrden.css'
 import Footer from '../components/Footer'
 import Header from '../components/Header'
 
