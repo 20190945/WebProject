c938c0f3fae21b7dc42d7646baa9e393aa80a72c
Robert213Z <114684008+Robert213Z@users.noreply.github.com>
2024-05-19 11:40:19 -0500

    PagPrincUsu.jsx

--- src/components/PagPrincUsu.jsx
+++ src/components/PagPrincUsu.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import '../Alumno3y4/PagPrincUsu.css'
+import '../components/PagPrincUsu.css'
 import Footer from '../components/Footer'
 import Header from '../components/Header'
 
