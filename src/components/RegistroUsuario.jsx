6729729a2d24cc1da1a8004279817729ff3755f0
Robert213Z <114684008+Robert213Z@users.noreply.github.com>
2024-05-19 11:42:16 -0500

    RegistroUsuario.jsx

--- src/components/RegistroUsuario.jsx
+++ src/components/RegistroUsuario.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import '../Alumno3y4/RegistroUsuario.css'
+import '../components/RegistroUsuario.css'
 import Footer from '../components/Footer'
 import Header from '../components/Header'
 
